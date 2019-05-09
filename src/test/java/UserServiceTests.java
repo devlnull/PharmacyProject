8bef20c74da6db2d21eb8659103d23163db633d9
amirhossein ghorbani <amirhosseinghorbani7@gmail.com>
2019-05-09 17:22:19 +0430

    add tests on user service class

---
+++ src/test/java/UserServiceTests.java
@@ -0,0 +1,52 @@
+package test.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+import com.pharmacy.context.IContext;
+import com.pharmacy.context.InMemoryContext;
+import com.pharmacy.entities.User;
+import com.pharmacy.repo.IRepo;
+import com.pharmacy.repo.InMemoryRepo;
+import com.pharmacy.services.IUserService;
+import com.pharmacy.services.UserService;
+
+public class UserServiceTests {
+
+    private IUserService getService() {
+        IContext context = new InMemoryContext();
+        IRepo<User> repo = new InMemoryRepo<>(context, User.class);
+        repo.Add(new User("test", "P@$$W0rd"));
+        return new UserService(repo);
+    }
+
+    @Test
+    public void Activate_CorrectUser_MustActivate(){
+        IUserService service = getService();
+        String username = "test";
+        boolean result = service.ActivateUser(username);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void Activate_InCorrectUser_MustNotActivate(){
+        IUserService service = getService();
+        String username = "test124";
+        boolean result = service.ActivateUser(username);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void Create_ExistUser_MustNotCreate(){
+        IUserService service = getService();
+        User result = service.CreateUser("test","P@$$W0rd");
+        Assert.assertNull(result);
+    }
+
+
+    @Test
+    public void Create_NotExistUser_MustCreate(){
+        IUserService service = getService();
+        User result = service.CreateUser("test1","P@$$W0rd");
+        Assert.assertNotNull(result);
+    }
+}
