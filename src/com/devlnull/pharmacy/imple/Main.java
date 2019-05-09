5b9c6c9f0d541a64a8edb37e09b393951a4ede5e
amirhossein ghorbani <amirhosseinghorbani7@gmail.com>
2019-05-09 09:15:17 +0430

    change main function after adding maven to project and use standard place of it.

--- src/com/devlnull/pharmacy/imple/Main.java
+++
@@ -1,9 +0,0 @@
-package com.devlnull.pharmacy.imple;
-
-public final class Main
-{
-  public static void main(final String[] args){
-    for(int i = 0; i < 10; i++)
-      System.out.println(String.format("test %d", i));
-  }
-}
