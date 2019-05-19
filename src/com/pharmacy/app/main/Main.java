1d0a2edb202fd5e8b71017c3918d42e871f2baf1
amirhossein ghorbani <amirhosseinghorbani7@gmail.com>
2019-05-20 00:15:26 +0430

    add javafx to project and some scenes to interact with application.

---
+++ src/com/pharmacy/app/main/Main.java
@@ -0,0 +1,23 @@
+package com.pharmacy.app.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Pharmacy Project");
+        primaryStage.setScene(new Scene(root, 1024, 960));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
