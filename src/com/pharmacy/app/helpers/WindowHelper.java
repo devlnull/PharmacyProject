07f4a874f05939a3fd61659d89414c66babe069f
amirhossein ghorbani <amirhosseinghorbani7@gmail.com>
2019-05-20 00:18:29 +0430

    add window helper to manage windows in javafx.

---
+++ src/com/pharmacy/app/helpers/WindowHelper.java
@@ -0,0 +1,27 @@
+package com.pharmacy.app.helpers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class WindowHelper {
+    public void show(URL location, String title, int width, int height){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(location);
+            Scene scene = new Scene(fxmlLoader.load(), width, height);
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new Window.", e);
+        }
+    }
+}
