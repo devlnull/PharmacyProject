8a968537e68be8c273a7ec75d1fee401bf49558c
amirhossein ghorbani <amirhosseinghorbani7@gmail.com>
2019-05-21 13:30:32 +0430

    new alert with error type.

--- src/com/pharmacy/app/helpers/AlertHelper.java
+++ src/com/pharmacy/app/helpers/AlertHelper.java
@@ -10,4 +10,12 @@ public class AlertHelper {
         alert.setContentText(content);
         alert.showAndWait();
     }
+
+    public static void GetErrorAlert(String title, String content){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
 }
